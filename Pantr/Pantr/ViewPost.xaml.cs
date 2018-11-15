6fe1e81608eea79d8e23c78519cc65d7fc00e9f9
Fredrik <fredrik0301@gmail.com>
2018-11-15 15:20:51 +0100

    pantopslag oversigt tilføjet + navigation

--- Pantr/Pantr/ViewPost.xaml.cs
+++ Pantr/Pantr/ViewPost.xaml.cs
@@ -12,9 +12,47 @@ namespace Pantr
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ViewPost : ContentPage
 	{
-		public ViewPost ()
+        public class Post
+        {
+            public int Id { get; set; }
+            public string Address { get; set; }
+            public string Quantity { get; set; }
+            public string Date { get; set; }
+            public string Time { get; set; }
+        }
+
+        Post post = new Post { Id = 1, Address = "Lygten 18, 2400 Kbh", Quantity = "1 Kasse", Date="11/12/2018", Time = "10.30-12.00" };
+        public ViewPost ()
 		{
+            BindingContext = post;
 			InitializeComponent ();
 		}
-	}
+
+        void OnImageNameTapped(object sender, EventArgs args)
+        {
+            try
+            {
+                DisplayAlert("test", "test", "test", "test");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private async void edit (object sender, EventArgs e)
+        {
+            var editPost = new EditPost();
+
+            await Navigation.PushModalAsync(editPost);
+
+            //DisplayAlert("test", "test", "test", "test");
+        }
+
+        private void submit_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("test", "", "test");
+
+        }
+    }
 }
