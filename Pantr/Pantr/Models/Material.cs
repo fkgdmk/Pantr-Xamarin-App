c5835cafad4017a070db39bc953aeb07b5780ba2
Fredrik <fredrik0301@gmail.com>
2018-11-23 11:49:41 +0100

    viewmodels tilføjet

---
+++ Pantr/Pantr/Models/Material.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pantr.Models
+{
+    public class Material
+    {
+        public string Type { get; set; }
+    }
+}
---
+++ Pantr/Pantr/Models/PostViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pantr.Models
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+        public string Material { get; set; }
+        public UserViewModel Giver { get; set; }
+        public string PostQuantity { get; set; }
+        public string Address { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public bool Claimed { get; set; }
+        public bool Completed { get; set; }
+        public string Date { get; set; }
+    }
+}
