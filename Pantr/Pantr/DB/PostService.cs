e37db81c9bb9ea5ab6f68349256579e135c8be2c
Fredrik <fredrik0301@gmail.com>
2018-11-26 14:46:55 +0100

    return async virker

--- Pantr/Pantr/DB/PostService.cs
+++ Pantr/Pantr/DB/PostService.cs
@@ -18,7 +18,7 @@ namespace Pantr.DB
                     HttpClient client = new HttpClient();
                     IEnumerable < PostViewModel > post = null;
 
-                    var uri = new Uri(string.Format("http://10.111.180.139:45457/api/posts"));
+                    var uri = new Uri(string.Format("http://10.111.180.139:45455/api/posts"));
 
                     var response = await client.GetAsync(uri);
 
@@ -31,8 +31,50 @@ namespace Pantr.DB
 
             TimeSpan.FromMinutes(352345235);
                 listView.ItemsSource = post;
-                }
+        }
+
+        public static async Task<PostViewModel> GetUsersPost()
+        {
+            //PostViewModel post = null;
+
+            HttpClient client = new HttpClient();
+            PostViewModel post = null;
+
+            var uri = new Uri(string.Format("http://10.111.180.122:45455/api/post/getuserspost/1"));
+
+            var response = await client.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                post = JsonConvert.DeserializeObject<PostViewModel>(content);
+               
+
+                // post.StartTime = FormatTime(post.StartTime);
+                //  post.EndTime = FormatTime(post.EndTime);
+               // string date = post.Date.ToString("dd/MM/yyyy");
+               // DateTime formatedDate = DateTime.ParseExact(date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+               ///post.Date = formatedDate
             }
+            return post;
+        }
+
+        public TimeSpan ConvertIntegerToTimeSpan(int minutesAfterMidnight)
+        {
+
+            int hours = minutesAfterMidnight / 60;
+            int minutes = minutesAfterMidnight % 60;
+
+            TimeSpan time = new TimeSpan(hours, minutes, 0);
+
+
+            //TimeSpan time = midnight.Add(hours)
+
+            return time;
+        }
+
+    }
+
 
 
 
