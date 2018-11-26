095a42d09cca68d476a8cafc1dd0cc28c5d041e4
AnderskdKnudsen <anderskdknudsen@gmail.com>
2018-11-26 12:03:47 +0100

    started interface

--- Pantr/Pantr/Login.xaml.cs
+++ Pantr/Pantr/Login.xaml.cs
@@ -8,15 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Pantr.Models;
 
 namespace Pantr
 {
-    public class LoginViewModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
@@ -42,45 +37,16 @@ namespace Pantr
             LoginViewModel login = new LoginViewModel()
             {
                 Username = username.Text,
-                Password = userPassword.Text
+                Password = HashString(userPassword.Text)
             };
-            var test = MethodAsync(login);
         }
 
-        public async Task MethodAsync(LoginViewModel login)
+        private string HashString(string inputString)
         {
-            Task<HttpResponseMessage> loginTest = LoginAsync(login);
-            var result = await loginTest;
-        }
-
-
-        public async Task<HttpResponseMessage> LoginAsync(LoginViewModel login, bool isNewItem = false)
-        {
-            HttpResponseMessage response = null;
-
-            using (HttpClient client = new HttpClient())
-            {
-                string url = @"http://localhost:50001/api/users";
-                var uri = new Uri(url);
-
-
-                var json = JsonConvert.SerializeObject(login);
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-                //if (isNewItem)
-                //{
-                response = await client.PostAsync(uri, content);
-                //}
-
-
-                if (response.IsSuccessStatusCode)
-                {
-                    Debug.WriteLine(@"                TodoItem successfully saved.");
-
-                }
-            }
-            return response;
-
+            byte[] data = Encoding.ASCII.GetBytes(inputString);
+            data = new System.Security.Cryptography.SHA256Managed().ComputeHash(data);
+            string hash = Encoding.ASCII.GetString(data);
+            return hash;
         }
     }
 }
