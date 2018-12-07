c916c30007190b9357f22585ebfa07ef68ccd75f
AnderskdKnudsen <anderskdknudsen@gmail.com>
2018-12-07 12:48:07 +0100

    haeh

--- Pantr/Pantr/App.xaml.cs
+++ Pantr/Pantr/App.xaml.cs
@@ -18,29 +18,6 @@ namespace Pantr
             //{
             //    MainPage = new NavigationPage(new Login());
             //}
-
-            //Kode forslag: PostsPage og CreatePostPage ?
-
-            MainPage = new GetPost();
-
-<<<<<<< HEAD
-=======
-            //MainPage = new NavigationPage(new ViewReservations());
-
-            //if (Current.Properties.ContainsKey("Username"))
-            //{
-            //        var username = Current.Properties["Username"] as string;
-            //MainPage = new NavigationPage(new ViewPost());
->>>>>>> 47f5b63d9f59055f3401021692360a0b21b549bf
-            //if (Current.Properties.ContainsKey("Username"))
-            //{
-            //    //    var username = Current.Properties["Username"] as string;
-            //    MainPage = new NavigationPage(new Posts());
-            //}
-            //else
-            //{
-            //    MainPage = new NavigationPage(new Login());
-            //}
             MainPage = new NavigationPage(new ViewReservations());
         }
 
